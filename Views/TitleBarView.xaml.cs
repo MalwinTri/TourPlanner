052fbfb555c1a4783b4529061c75bdeff501ba86
davidf <david@froeschl.com>
2025-03-13 14:51:52 +0100

    TitleBarView ausgelagert

---
+++ Views/TitleBarView.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace TourPlanner.Views
+{
+    public partial class TitleBarView : UserControl
+    {
+        public TitleBarView()
+        {
+            InitializeComponent();
+        }
+    }
+}
