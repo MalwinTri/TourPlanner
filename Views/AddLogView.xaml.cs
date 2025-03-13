dca918ad2cb2b1eba4fe413ed11fa3c75bcaa8d6
MalwinTri <trimmel.m02@gmail.com>
2025-03-13 13:31:44 +0100

    Adding new Designs

---
+++ Views/AddLogView.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TourPlanner.Views
+{
+    /// <summary>
+    /// Interaction logic for AddLogView.xaml
+    /// </summary>
+    public partial class AddLogView : UserControl
+    {
+        public AddLogView()
+        {
+            InitializeComponent();
+        }
+
+        private void NumericOnly(object sender, TextCompositionEventArgs e)
+        {
+            // Erlaubt nur Ziffern 0-9
+            e.Handled = !Regex.IsMatch(e.Text, "^[0-9]*$");
+        }
+    }
+}
---
+++ Views/ToursListView.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TourPlanner.ViewModels
+{
+    public class ToursListViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<string> _tours;
+
+        public ObservableCollection<string> Tours
+        {
+            get => _tours;
+            set
+            {
+                _tours = value;
+                OnPropertyChanged(nameof(Tours));
+            }
+        }
+
+        public ToursListViewModel()
+        {
+            // Beispiel-Daten, könnten aus einer Datenbank geladen werden
+            Tours = new ObservableCollection<string>
+            {
+                "Wienerwald",
+                "Dopperlhütte",
+                "Figlwarte",
+                "Dorfrunde"
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
